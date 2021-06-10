95f5b9319938e34cbf2f1f70d75276fe6290f1cd
Iresharma <iresh.sharma8@gmail.com>
2021-06-10 14:58:20 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by iresh sharma on 10/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
